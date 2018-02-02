4ee54baa38b64c57b77d2c4fb228512d218d33f2
Kenan Ekici <kenan.ekici@outlook.com>
2018-02-02 17:04:49 +0100

    Base app

--- api/middleware/middleware.js
+++
@@ -1,8 +0,0 @@
-var models = require('./../models');
-var User = models.User;
-
-function requiresUser(req, res, next) {
-    res.app.oauth.authorise()(req, res, next);
-}
-
-module.exports.requiresUser = requiresUser;
